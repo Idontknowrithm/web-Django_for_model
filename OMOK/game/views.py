9481dacaa77e0e608098778e0f7d6508ef4eaab3
김유현 <sustkadl12@gmail.com>
2022-03-20 06:44:56 +0000

    2021-03-20 updates

--- OMOK/game/views.py
+++ OMOK/game/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.views import templates
+from django.core import serializers
+from django.views.decorators.csrf import csrf_exempt
+from .api.omok import Omok
 
+import json
 
+@csrf_exempt
 def index(request):
-    return HttpResponse("index page")
+    context = {}
+    return render(request, 'index.html', context)
